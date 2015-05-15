f87061dd30392e96cab61316fb13e321edc6fcdb
Magnus Lindberg <magnus.m.lindberg@gmail.com>
2015-05-15 19:55:54 +0200

    Changed a comment

--- test/com/mlindb/markupbuilder/TagTest.java
+++ test/com/mlindb/markupbuilder/TagTest.java
@@ -5,9 +5,25 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * Created by mlindberg on 14/05/15.
+/*
+ * Copyright 2015 Magnus Lindberg - magnus.m.lindberg@gmail.com
+ *
+ * This file is part of MarkupBuilder.
+ *
+ * MarkupBuilder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MarkupBuilder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MarkupBuilder.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 public class TagTest {
     private Tag a, br, body, p, span, img, h, none;
 
